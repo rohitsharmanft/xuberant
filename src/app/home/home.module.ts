46038c5fdfe1cf684916542babca12548960b995
rohitsharmanft <rohitsharmanft@gmail.com>
2026-03-17 15:28:42 +0530

    chore: align capacitor package majors and remove unused viewer deps

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,16 +3,8 @@ import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
-import { Routes, RouterModule } from '@angular/router';
 import { HomePageRoutingModule } from './home-routing.module';
 import { FileUploadModule } from 'ng2-file-upload';
-
-const routes: Routes = [
-  {
-    path: '',
-    component: HomePage
-  }
-];
 
 
 @NgModule({
@@ -21,8 +13,8 @@ const routes: Routes = [
     FormsModule,
     IonicModule,
     HomePageRoutingModule,
-	FileUploadModule
+    FileUploadModule,
   ],
-  declarations: [HomePage]
+  declarations: [HomePage],
 })
 export class HomePageModule {}
